6bf7be0d03135c9a50a781768b402ed1773c3943
Iván Álvarez López <ial.navy@protonmail.ch>
2023-06-08 20:05:32 +0200

    Bug: SheetRepository.mjs was referencing to a wrong collection

--- persistenceLayer/entities/SheetRepository.mjs
+++ persistenceLayer/entities/SheetRepository.mjs
@@ -2,7 +2,7 @@ import {AbstractRepository} from "../AbstractRepository.mjs";
 
 class SheetRepository extends AbstractRepository {
     constructor(app, mongoClient) {
-        super(app, mongoClient, "sheet");
+        super(app, mongoClient, "sheets");
     }
 }
 
