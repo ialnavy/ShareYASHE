01d733c3f6ce4804fce83690002831db850c4423
Iván Álvarez López <ial.navy@protonmail.ch>
2023-06-08 20:17:01 +0200

    JS & CSS dependencies only on views which depend on them

--- presentationLayer/viewEngine/sheet/CreateSheetRender.mjs
+++ presentationLayer/viewEngine/sheet/CreateSheetRender.mjs
@@ -4,12 +4,6 @@ class CreateSheetRender extends PO_Render {
     constructor(request, response, logicFactory) {
         super(request, response, logicFactory, 'sheet/createSheet.pug', PO_Render.WEBSITE_NAME.concat(' - Create sheet'));
     }
-    async _getAdditionalRenderParameters(renderParameters) {
-        // This parameter is used to import dynamically the requested libraries by YASHE
-        renderParameters['requestedScripts'] = ['simpleyashe.js'];
-
-        return renderParameters;
-    }
 }
 
 export {CreateSheetRender};
--- presentationLayer/viewEngine/sheet/SheetRender.mjs
+++ presentationLayer/viewEngine/sheet/SheetRender.mjs
@@ -4,13 +4,6 @@ class SheetRender extends PO_Render {
     constructor(request, response, logicFactory) {
         super(request, response, logicFactory, 'sheet/sheet.pug', PO_Render.WEBSITE_NAME.concat(' - Edit sheet'));
     }
-
-    async _getAdditionalRenderParameters(renderParameters) {
-        // This parameter is used to import dynamically the requested libraries by YASHE
-        renderParameters['requestedScripts'] = ['shareyashe.js'];
-
-        return renderParameters;
-    }
 }
 
 export {SheetRender};
